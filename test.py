68457942d042da591b50688a6f9001ddb650b22b
WenKangChua <88809920+WenKangChua@users.noreply.github.com>
2026-03-02 22:51:10 +0800

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import torch
-if torch.backends.mps.is_available():
-    mps_device = torch.device("mps")
-    x = torch.ones(1, device=mps_device)
-    print("Success! Tensor on M2 GPU:", x)
-else:
-    print("MPS device not found. Check your macOS and PyTorch versions.")
